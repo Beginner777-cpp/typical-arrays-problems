a50a996840973e7cbee16d8933a03aab26822fd7
Khusanjon Abdigafurov <abdigafurovhusan@gmail.com>
2021-03-15 00:30:00 +0500

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let res = array[0];
+  array.forEach(element => {
+    if (res > element) {
+      res = element;
+    }
+  });
+
+  return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let res = array[0];
+  array.forEach(element => {
+    if (res < element) {
+      res = element;
+    }
+  });
+
+  return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array) {
+    return 0;
+  }
+  if (array.length == 0) {
+    return 0;
+  }
+  let res = 0;
+  array.forEach(element => {
+    res += element;
+  });
+
+  return res/array.length;
 }
